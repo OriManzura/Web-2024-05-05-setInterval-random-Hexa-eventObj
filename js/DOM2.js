e7b584e42f1f12a8e2a540366a24e699f26a17d3
NatalieShif <natalia.shif@gmail.com>
2024-05-05 16:37:09 +0300

    old homework

--- js/DOM2.js
+++ js/DOM2.js
@@ -5,14 +5,19 @@ const btnSalmon = document.querySelector('.salmon')
 
 const out1 = document.querySelector('#out1')
 
+// Event object - is an object that consists of the 
+// properties that describe the event that happened
 function changeColor(event) {
 
     console.log(event.target)
 
+    let color = event.target.innerText.toLowerCase();
+
     out1.classList.remove('blue');
     out1.classList.remove('green');
     out1.classList.remove('salmon');
-    out1.classList.add('yellow');
+    out1.classList.remove('yellow');
+    out1.classList.add(color);
 
 }
 
