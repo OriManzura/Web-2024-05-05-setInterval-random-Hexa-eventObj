39701586fc711aac04dd663f6dc8d35defc173ba
NatalieShif <natalia.shif@gmail.com>
2024-05-05 15:54:40 +0300

    event object

--- js/DOM2.js
+++ js/DOM2.js
@@ -5,4 +5,20 @@ const btnSalmon = document.querySelector('.salmon')
 
 const out1 = document.querySelector('#out1')
 
+function changeColor(event) {
 
+    console.log(event.target)
+
+    out1.classList.remove('blue');
+    out1.classList.remove('green');
+    out1.classList.remove('salmon');
+    out1.classList.add('yellow');
+
+}
+
+btnYellow.addEventListener('click',changeColor);
+btnBlue.addEventListener('click',changeColor);
+btnGreen.addEventListener('click',changeColor);
+btnSalmon.addEventListener('click',changeColor);
+
+// ar.forEach((elem,idx) => {})
